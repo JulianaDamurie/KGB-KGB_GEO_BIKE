e91a130e2bbd0bfb47426cdffe9dccf304c41474
Unknown <jnds@cin.ufpe.br>
2018-07-10 01:13:53 -0300

    adicionando api knot python

---
+++ Gateway/api/examples/setThing.py
@@ -0,0 +1,19 @@
+from knotpy import *
+from credentials import *
+conn = KnotConnection('socketio', credentials)
+
+myThings = conn.getThings()
+
+for thing in myThings:
+	print(thing)
+	print(60*'-')
+	print('DATA')
+	data = conn.getData(thing['uuid'], limit=1)
+	conn.setData(thing['uuid'],1,True)
+	print data
+	'''print('Set data')
+	if thing.get('schema'):
+		for sensor in thing.get('schema'):
+			if sensor['name'] == 'LED':
+				conn.setData(thing['uuid'], sensor['sensor_id'], True)
+	print(60*'*')'''
